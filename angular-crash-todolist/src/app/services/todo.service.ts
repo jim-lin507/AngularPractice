6ed31b27425e1cbf8447dd365e42cbc7a56ab38c
Jim Lin <kangxianlin@gmail.com>
2019-10-28 09:11:19 +0800

    Created angular-crash-todolist

---
+++ angular-crash-todolist/src/app/services/todo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor() { }
+
+  getTodos()
+  {
+    return [
+      {
+        id:1,
+        title:"Todo one",
+        completed:false
+      },
+      {
+        id:2,
+        title:"Todo Two",
+        completed:false
+      }
+      ,
+      {
+        id:3,
+        title:"Todo Three",
+        completed:true
+      }
+    ]
+  }
+}
